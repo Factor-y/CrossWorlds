a45a46a89bd7ea5b6512c3248a107c4e23f22e7f
Paul Withers <paulswithers@hotmail.co.uk>
2015-03-18 14:48:29 +0000

    Adding j2eeenabler to pom.xml Amended ConferenceUI to display Tracks (not sure how to create new pages and map, or if we will)

--- samples/ConferenceApp/src/org/openntf/conferenceapp/ui/ConferenceUI.java
+++ samples/ConferenceApp/src/org/openntf/conferenceapp/ui/ConferenceUI.java
@@ -1,6 +1,12 @@
 package org.openntf.conferenceapp.ui;
 
-import java.util.Date;
+import java.util.List;
+
+import org.openntf.conference.graph.Track;
+import org.openntf.conferenceapp.service.TrackFactory;
+import org.openntf.domino.Session;
+import org.openntf.domino.utils.Factory;
+import org.openntf.domino.utils.Factory.SessionType;
 
 import com.vaadin.annotations.Theme;
 import com.vaadin.event.ItemClickEvent;
@@ -18,25 +24,37 @@ public class ConferenceUI extends UI {
 	protected void init(VaadinRequest request) {
 
 		getPage().setTitle("[OpenNTF] Conferences");
-		
+
 		VerticalLayout vert = new VerticalLayout();
-		
+
 		Table sessionTable = new Table();
-		
-		sessionTable.addContainerProperty("Date", Date.class, null);
-		sessionTable.addContainerProperty("Session", String.class, null);
-		sessionTable.addContainerProperty("Speaker", String.class, null);
-		
-		sessionTable.addItem(new Object[] { new Date(), "dev","Daniele Vistalli"},  "DV Session");
-		sessionTable.addItem(new Object[] { new Date(), "dev","Paul Withers"},  "PW Session");
+
+		Session sess = Factory.getSession(SessionType.CURRENT);
+		System.out.println(sess.getEffectiveUserName());
+
+		// sessionTable.addContainerProperty("Date", Date.class, null);
+		// sessionTable.addContainerProperty("Session", String.class, null);
+		// sessionTable.addContainerProperty("Speaker", String.class, null);
+		sessionTable.addContainerProperty("Title", String.class, null);
+		sessionTable.addContainerProperty("Description", String.class, null);
+
+		List<Track> tracks = TrackFactory.getTracksSortedByProperty("Title");
+		for (Track t : tracks) {
+			sessionTable.addItem(new Object[] { t.getTitle(), t.getDescription() }, t.getTitle());
+		}
+
+		// sessionTable.addItem(new Object[] { new Date(),
+		// "dev","Daniele Vistalli"}, "DV Session");
+		// sessionTable.addItem(new Object[] { new Date(),
+		// "dev","Paul Withers"}, "PW Session");
 
 		sessionTable.setSizeFull();
 		sessionTable.addItemClickListener(new ItemClickListener() {
-			
+
 			public void itemClick(ItemClickEvent event) {
-				
+
 				System.out.println(event.getItemId() + " clicked");
-				
+
 			}
 		});
 
@@ -44,7 +62,7 @@ public class ConferenceUI extends UI {
 		vert.setSizeFull();
 
 		setContent(vert);
-		
+
 	}
 
 }
