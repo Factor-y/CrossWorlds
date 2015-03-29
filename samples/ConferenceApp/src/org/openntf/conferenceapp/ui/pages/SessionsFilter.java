43a81a8a98b2c5b5daf2309d76cc850716f03d11
Paul Withers <paulswithers@hotmail.co.uk>
2015-03-29 19:00:53 +0100

    SessionFilter added. Still need to enable filtering and add Track

--- samples/ConferenceApp/src/org/openntf/conferenceapp/ui/pages/SessionsFilter.java
+++ samples/ConferenceApp/src/org/openntf/conferenceapp/ui/pages/SessionsFilter.java
@@ -1,23 +1,31 @@
 package org.openntf.conferenceapp.ui.pages;
 
+import java.text.DateFormatSymbols;
+import java.util.Date;
+import java.util.Iterator;
+
 import org.openntf.conference.graph.Location;
 import org.openntf.conference.graph.Track;
 import org.openntf.conferenceapp.service.LocationFactory;
 import org.openntf.conferenceapp.service.TrackFactory;
+import org.openntf.conferenceapp.ui.ConferenceUI;
 
+import com.vaadin.data.Item;
+import com.vaadin.data.util.IndexedContainer;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.Resource;
-import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.MenuBar;
 import com.vaadin.ui.MenuBar.MenuItem;
 import com.vaadin.ui.Notification;
 import com.vaadin.ui.Notification.Type;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.ValoTheme;
 
-public class SessionsFilter extends CssLayout implements View {
+public class SessionsFilter extends VerticalLayout implements View {
 
 	/**
 	 * 
@@ -25,7 +33,8 @@ public class SessionsFilter extends CssLayout implements View {
 	private static final long serialVersionUID = 1L;
 	public static final String VIEW_NAME = "SessionsFilter";
 	public static final String VIEW_DESC = "Sessions By...";
-	private TraditionalGrid grid;
+	private PresentationsContainer presentations;
+	private VerticalLayout details;
 
 	public SessionsFilter() {
 
@@ -34,10 +43,8 @@ public class SessionsFilter extends CssLayout implements View {
 	public void loadContent() {
 		try {
 
-			setSizeFull();
-
 			MenuBar menubar = new MenuBar();
-			menubar.addStyleName(ValoTheme.MENU_SUBTITLE);
+			menubar.setStyleName(ValoTheme.MENU_SUBTITLE);
 			menubar.setWidth(100, Unit.PERCENTAGE);
 			menubar.addItem("Sessions By...", null);
 			MenuItem tracks = menubar.addItem("Filter by Track", null, null);
@@ -55,14 +62,56 @@ public class SessionsFilter extends CssLayout implements View {
 			days.addItem("March 30, 2015", dayFilterCommand);
 			days.addItem("March 31, 2015", dayFilterCommand);
 
+			presentations = new PresentationsContainer();
+
 			addComponent(menubar);
 
-			HorizontalLayout sessionRow = new HorizontalLayout();
-			sessionRow.setSizeFull();
-			addComponent(sessionRow);
+			loadData();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	public void loadData() {
+		String catLabel = "";
+		DateFormatSymbols s = new DateFormatSymbols(UI.getCurrent().getLocale());
+		String[] days = s.getShortWeekdays();
+
+		details = new VerticalLayout();
+		IndexedContainer table = presentations.getContainer();
+		table.sort(new Object[] { "StartTime" }, new boolean[] { true });
+
+		// Iterate over the item identifiers of the table.
+		for (Iterator i = table.getItemIds().iterator(); i.hasNext();) {
+			// Get the current item identifier, which is an integer.
+			int iid = (Integer) i.next();
+
+			// Now get the actual item from the
+			Item item = table.getItem(iid);
+
+			Date sTime = (Date) item.getItemProperty("StartTime").getValue();
+			Date eTime = (Date) item.getItemProperty("EndTime").getValue();
+			String tmpCatLabel = days[sTime.getDay() + 1] + " " + ConferenceUI.TIME_FORMAT.format(sTime) + "-"
+					+ ConferenceUI.TIME_FORMAT.format(eTime);
+			if (!tmpCatLabel.equals(catLabel)) {
+				Label cat = new Label(tmpCatLabel);
+				cat.setStyleName(ValoTheme.LABEL_H3);
+				details.addComponent(cat);
+				catLabel = tmpCatLabel;
+			}
+
+			// Add track!
+
+			VerticalLayout sessionDetails = new VerticalLayout();
+			sessionDetails.setWidth(100, Unit.PERCENTAGE);
+			Label title = new Label(item.getItemProperty("SessionID").getValue() + " - " + item.getItemProperty("Title").getValue());
+			sessionDetails.addComponent(title);
+			sessionDetails.addComponent(new Label(item.getItemProperty("Speakers").getValue() + " (" + item.getItemProperty("Location").getValue()
+					+ ")"));
+			details.addComponent(sessionDetails);
+		}
+
+		addComponent(details);
 	}
 
 	public Resource getIcon(String trackCode) {
@@ -85,7 +134,7 @@ public class SessionsFilter extends CssLayout implements View {
 
 	MenuBar.Command trackFilterCommand = new MenuBar.Command() {
 		public void menuSelected(MenuItem selectedItem) {
-			System.out.println(selectedItem.getText());
+
 		}
 	};
 
